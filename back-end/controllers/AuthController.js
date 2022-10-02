a3ff3c1f27d01164f7f290e04df4e9056a6113e9
Nhut Kha <nhutkha2410@gmail.com>
2022-10-02 14:26:11 +0700

    add auth api

--- back-end/controllers/AuthController.js
+++ back-end/controllers/AuthController.js
@@ -1,25 +1,39 @@
-const Staff = require('../models/Staff');
+const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res) => {
   try {
-    const { firstName, lastName, username, password, ID, birthday, role } = req.body;
-    // Hash passwords 
+    const {
+      avatar,
+      fullName,
+      username,
+      password,
+      email,
+      phoneNumber,
+      createdAt,
+      officerCode,
+      birthday,
+      role,
+    } = req.body;
+    // Hash passwords
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const staff = {
-      firstName: firstName,
-      lastName: lastName,
+    const user = {
+      avatar: avatar,
+      fullName: fullName,
       username: username,
       password: hashedPassword,
-      ID: ID,
+      email: email,
+      phoneNumber: phoneNumber,
+      createdAt: createdAt,
+      officerCode: officerCode,
       birthday: birthday,
       role: role,
-    }
+    };
 
-    const response = await Staff.create(staff);
+    const response = await User.create(user);
     res.status(200).json(response);
   } catch (err) {
     res.status(400).json({ message: err });
@@ -27,37 +41,41 @@ exports.register = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
-  let step = 0;
   try {
     const { username, password } = req.body;
-    if(!username || !password) {
-      return res.status(404).json({ message: "Username or Password is required !" });
+    if (!username || !password) {
+      return res
+        .status(404)
+        .json({ message: 'Username or Password is required !' });
     }
-    step = 1
-    const staff = await Staff.findOne({ username: username }).populate('role');
-    step = staff || 2
-    if (!staff) {
-      return res.status(404).json({ message: "Username or Password is wrong !" });
+    const user = await User.findOne({ username: username }).populate('role');
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: 'Username or Password is wrong !' });
     }
-    
-    const validPassword = await bcrypt.compare(password, staff.password);
+
+    const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
-      return res.status(404).json({ message: "Username or Password is wrong !" });
+      return res
+        .status(404)
+        .json({ message: 'Username or Password is wrong !' });
     }
-    step = 3
-    const token = await jwt.sign({_id: staff._id}, process.env.ACCESS_TOKEN_SECRET);
-    return  res.header('auth-token', token).send({token: token, staff: staff});
-    
+    const token = await jwt.sign(
+      { _id: user._id },
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    return res.header('auth-token', token).send({ token: token, user: user });
   } catch (err) {
-    return res.status(400).json({ message: err, body: req.body, step: step });
+    return res.status(400).json({ message: err, body: req.body });
   }
 };
 
 exports.getAccount = async (req, res) => {
-  const staff = await Staff.findOne({ _id: req.params.id }).populate('role');
-  if (!staff) {
-    return res.status(404).json({ message: "Not Found !" });
+  const user = await User.findOne({ _id: req.params.id }).populate('role');
+  if (!user) {
+    return res.status(404).json({ message: 'Not Found !' });
   }
 
-  return res.status(200).json(staff);
-}
+  return res.status(200).json(user);
+};
