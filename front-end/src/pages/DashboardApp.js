d6b05810066d6a03375def0a519d4751a1858951
thanh <thanhctag1999@gmail.com>
2022-10-28 14:34:43 +0700

    update lateTask

--- front-end/src/pages/DashboardApp.js
+++ front-end/src/pages/DashboardApp.js
@@ -1,30 +1,18 @@
-import { faker } from '@faker-js/faker';
 // @mui
+import { Container, Grid, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-import { Grid, Container, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-
-import taskApi from '../api/taskApi';
-import userApi from '../api/userApi';
-import taskTypeApi from '../api/taskTypeApi';
 import roleApi from '../api/roleApi';
+import taskApi from '../api/taskApi';
+import taskTypeApi from '../api/taskTypeApi';
+import userApi from '../api/userApi';
 
 // components
 import Page from '../components/Page';
-import Iconify from '../components/Iconify';
 // sections
-import {
-  AppTasks,
-  AppNewsUpdate,
-  AppOrderTimeline,
-  AppCurrentVisits,
-  AppWebsiteVisits,
-  AppTrafficBySite,
-  AppWidgetSummary,
-  AppCurrentSubject,
-  AppConversionRates,
-} from '../sections/@dashboard/app';
+import { AppConversionRates, AppCurrentVisits, AppWebsiteVisits, AppWidgetSummary } from '../sections/@dashboard/app';
+import LateTask from '../sections/@dashboard/app/LateTask';
+
 // ----------------------------------------------------------------------
 
 export default function DashboardApp() {
@@ -155,42 +143,29 @@ export default function DashboardApp() {
 
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={3}>
-            <Link className="no-underline" to="/dashboard/task">
-              <AppWidgetSummary
-                title="Công việc"
-                total={tasks.length}
-                color="success"
-                icon={'ant-design:code-sandbox-outlined'}
-              />
-            </Link>
+            <AppWidgetSummary
+              title="Công việc"
+              total={tasks.length}
+              color="success"
+              icon={'ant-design:code-sandbox-outlined'}
+            />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
-            <Link className="no-underline" to="/dashboard/user">
-              <AppWidgetSummary title="Người dùng" total={users.length} icon={'ant-design:user-outlined'} />
-            </Link>
+            <AppWidgetSummary title="Người dùng" total={users.length} icon={'ant-design:user-outlined'} />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
-            <Link className="no-underline" to="/dashboard/role">
-              <AppWidgetSummary
-                title="Vai trò"
-                total={roles.length}
-                color="warning"
-                icon={'ant-design:star-outlined'}
-              />
-            </Link>
+            <AppWidgetSummary title="Vai trò" total={roles.length} color="warning" icon={'ant-design:star-outlined'} />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
-            <Link className="no-underline" to="/dashboard/type-task">
-              <AppWidgetSummary
-                title="Loại công việc"
-                total={taskType.length}
-                color="error"
-                icon={'ant-design:appstore-outlined'}
-              />
-            </Link>
+            <AppWidgetSummary
+              title="Loại công việc"
+              total={taskType.length}
+              color="error"
+              icon={'ant-design:appstore-outlined'}
+            />
           </Grid>
 
           <Grid item xs={12} md={6} lg={8}>
@@ -248,21 +223,7 @@ export default function DashboardApp() {
           </Grid>
 
           <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentVisits
-              title="Thống kê danh mục"
-              chartData={[
-                { label: 'Công việc', value: tasks.length },
-                { label: 'Người dùng', value: users.length },
-                { label: 'Vai trò', value: roles.length },
-                { label: 'Loại công việc', value: taskType.length },
-              ]}
-              chartColors={[
-                theme.palette.primary.main,
-                theme.palette.chart.red[0],
-                theme.palette.chart.violet[0],
-                theme.palette.chart.green[0],
-              ]}
-            />
+            <LateTask title="Thống kê công việc trễ hạn" subheader="Số lượng công việc sắp đến hạn" tasks={tasks} />
           </Grid>
 
           <Grid item xs={12} md={6} lg={8}>
@@ -279,86 +240,19 @@ export default function DashboardApp() {
           </Grid>
 
           <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentSubject
-              title="Current Subject"
-              chartLabels={['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math']}
+            <AppCurrentVisits
+              title="Thống kê danh mục"
               chartData={[
-                { name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
-                { name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
-                { name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
+                { label: 'Công việc', value: tasks.length },
+                { label: 'Người dùng', value: users.length },
+                { label: 'Vai trò', value: roles.length },
+                { label: 'Loại công việc', value: taskType.length },
               ]}
-              chartColors={[...Array(6)].map(() => theme.palette.text.secondary)}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
-            <AppNewsUpdate
-              title="News Update"
-              list={[...Array(5)].map((_, index) => ({
-                id: faker.datatype.uuid(),
-                title: faker.name.jobTitle(),
-                description: faker.name.jobTitle(),
-                image: `/static/mock-images/covers/cover_${index + 1}.jpg`,
-                postedAt: faker.date.recent(),
-              }))}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppOrderTimeline
-              title="Order Timeline"
-              list={[...Array(5)].map((_, index) => ({
-                id: faker.datatype.uuid(),
-                title: [
-                  '1983, orders, $4220',
-                  '12 Invoices have been paid',
-                  'Order #37745 from September',
-                  'New order placed #XF-2356',
-                  'New order placed #XF-2346',
-                ][index],
-                type: `order${index + 1}`,
-                time: faker.date.past(),
-              }))}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppTrafficBySite
-              title="Traffic by Site"
-              list={[
-                {
-                  name: 'FaceBook',
-                  value: 323234,
-                  icon: <Iconify icon={'eva:facebook-fill'} color="#1877F2" width={32} height={32} />,
-                },
-                {
-                  name: 'Google',
-                  value: 341212,
-                  icon: <Iconify icon={'eva:google-fill'} color="#DF3E30" width={32} height={32} />,
-                },
-                {
-                  name: 'Linkedin',
-                  value: 411213,
-                  icon: <Iconify icon={'eva:linkedin-fill'} color="#006097" width={32} height={32} />,
-                },
-                {
-                  name: 'Twitter',
-                  value: 443232,
-                  icon: <Iconify icon={'eva:twitter-fill'} color="#1C9CEA" width={32} height={32} />,
-                },
-              ]}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
-            <AppTasks
-              title="Tasks"
-              list={[
-                { id: '1', label: 'Create FireStone Logo' },
-                { id: '2', label: 'Add SCSS and JS files if required' },
-                { id: '3', label: 'Stakeholder Meeting' },
-                { id: '4', label: 'Scoping & Estimations' },
-                { id: '5', label: 'Sprint Showcase' },
+              chartColors={[
+                theme.palette.primary.main,
+                theme.palette.chart.red[0],
+                theme.palette.chart.violet[0],
+                theme.palette.chart.green[0],
               ]}
             />
           </Grid>
